cb2798a5a32c9a08da66523bbb0dfe4779535b48
Seungbo Shim <112371013+seungboshim@users.noreply.github.com>
2025-02-16 20:03:50 +0900

    [Silver III] Title: 타노스, Time: 92 ms, Memory: 108384 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,7 +1,8 @@
 import sys
 input = sys.stdin.readline
 
-S = input()
+S = list(input().strip())
+length = len(S)
 
 zero = 0
 one = 0
@@ -9,8 +10,27 @@ for s in S:
   if s == '0': zero+=1
   else: one+=1
 
-zero //= 2
-one //= 2
+left = 0
+zerodel = 0
+onedel = 0
+delIdx = []
+for left in range(len(S)):
+  if zerodel == zero/2 and onedel == one/2:
+    break
+  
+  right = len(S)-left-1
+  # print(left,right,S)
 
-result = '0'*zero + '1'*one
-print(result)
+  if S[left] == '1' or S[right] == '0':
+    if S[left] == '1' and onedel < one/2:
+      delIdx.append(left)
+      onedel+=1
+    if S[right] == '0' and zerodel < zero/2:
+      delIdx.append(right)
+      zerodel+=1
+
+delIdx.sort(reverse=True)
+for idx in delIdx:
+  del S[idx]
+
+print(''.join(S))
